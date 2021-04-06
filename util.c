b1dfa7eb7369e9b4323528cebabd951793178b27
Flavius Gorgonio <flgorgonio@gmail.com>
2021-04-06 18:27:19 -0300

    Navegação do Menu Principal

--- util.c
+++ util.c
@@ -5,6 +5,10 @@
 #include <time.h>
 #include <stdlib.h>
 
+
+///////////////////////////////////////////////////////////////////////////////
+/// Faz uma pausa por n segundos, com n sendo passado como parâmetro
+///
 void delay(int segundos) {
   int tempo = 1000 * segundos;
   clock_t inicio = clock();
@@ -14,6 +18,9 @@ void delay(int segundos) {
 }
 
 
+///////////////////////////////////////////////////////////////////////////////
+/// Limpa a tela; funciona em Linux / MacOS / Windows
+///
 void limpaTela(void) {
   if (system("clear") || system("cls")) {
 
@@ -21,6 +28,10 @@ void limpaTela(void) {
 }
 
 
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se o caractere recebido for um dígito (entre 0 e 9)
+/// retorna 0 caso contrário
+///
 int ehDigito(char c) {
   if (c >= '0' && c <= '9') {
     return 1;
@@ -29,3 +40,53 @@ int ehDigito(char c) {
   }
 }
 
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se o caractere recebido for uma alfabético 
+/// (letra entre 'A' e 'Z' ou 'a' e 'z') ou retorna 0 caso contrário
+///
+int ehLetra(char c) {
+  if (c >= 'A' && c <= 'Z') {
+    return 1;
+  } else if (c >= 'a' && c <= 'z') {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se string recebido for exclusivamente alfabético ou
+/// retorna 0 caso contrário
+///
+int validarNome(char* nome) {
+	return 1;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se string recebido for endereço de email válido ou
+/// retorna 0 caso contrário
+///
+int validarEmail(char* email) {
+	return 1;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se string recebido corresponder a uma data válida (apenas dígitos
+/// e no formato: ddmmaaaa) ou retorna 0 caso contrário
+///
+int validarData(char* data) {
+	return 1;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se string recebido corresponder a um número de celular válido 
+/// (apenas dígitos) ou retorna 0 caso contrário
+///
+int validarFone(char* fone) {
+	return 1;
+}
